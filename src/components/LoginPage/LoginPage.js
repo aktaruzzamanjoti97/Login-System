1cd94390578571a5efc855a561e19320c331d0e3
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-06-07 21:42:18 +0600

    fully done

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -13,12 +13,6 @@ if (!firebase.apps.length) {
 }
 
 const LoginPage = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
 
   const [user, setUser] = useState({
     name: "",
@@ -28,27 +22,21 @@ const LoginPage = () => {
   });
   const [newUser, setNewUser] = useState(false);
 
-  const onSubmit = (data) => {
-    console.log(data);
-    if (newUser && user.password !== user.confirmPassword) {
-      alert("Password is incorrect");
-    }
-  };
 
   const handleBlur = (e) => {
     let isFieldValid = true;
     if (e.target.name === "email") {
       isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
     }
-    if ((e.target.name = "password")) {
-      const isPasswordValid = e.target.value.length > 7;
+    if ((e.target.name === "password")) {
+      const isPasswordValid = e.target.value.length > 6;
       const passwordHasNumber = /\d{1}/.test(e.target.value);
       isFieldValid = isPasswordValid && passwordHasNumber;
     }
-    if ((e.target.name = "confirmPassword")) {
-      const isConfirmPasswordValid = e.target.value.length > 7;
+    if ((e.target.name === "confirmPassword")) {
+      const isConfirmPasswordValid = e.target.value.length > 6;
       const isConfirmPasswordHasNumber = /\d{1}/.test(e.target.value);
-      isFieldValid = isConfirmPasswordHasNumber && isConfirmPasswordHasNumber;
+      isFieldValid = isConfirmPasswordValid && isConfirmPasswordHasNumber;
     }
     if (isFieldValid) {
       const newUserInfo = { ...user };
@@ -57,109 +45,143 @@ const LoginPage = () => {
     }
   };
 
+  const handleSubmit = (e) => {
+    // console.log(data);
+    if (newUser && user.password !== user.confirmPassword) {
+      alert("Password is incorrect");
+    }
+    if (newUser && user.email && user.password === user.confirmPassword) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(user.email, user.password)
+        .then((userCredential) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = "";
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+        });
+    }
+    if (!newUser && user.email && user.password) {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(user.email, user.password)
+        .then((userCredential) => {
+          //Signed in
+          const newUserInfo = { ...user };
+          newUserInfo.error = "";
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+        });
+    }
+    e.preventDefault();
+  };
+
   return (
     <div className="container">
       <h1 className="m-5 text-warning">DOOF</h1>
       <div className="row">
         <div className="col-md-7 offset-md-1">
           <h3>{newUser ? "Sign Up" : "Log In"}</h3>
-          <>
-            {!newUser && (
-              <div>
-                <form onSubmit={handleSubmit(onSubmit)}>
+
+          <div>
+            <form onSubmit={handleSubmit}>
+              {newUser && (
+                <div>
                   <label htmlFor="email">Email</label>
                   <input
-                    {...register("email")}
-                    type="email"
-                    onBlur={handleBlur}
-                    placeholder="email"
                     className="form-control"
-                    required
+                    onBlur={handleBlur}
+                    name="newUser"
+                    type="text"
+                    placeholder="Enter Your Name"
                   />
-                  <label htmlFor="email">Password</label>
+                </div>
+              )}{" "}
+              <label htmlFor="email">Email</label>
+              <input
+                name="email"
+                type="email"
+                onBlur={handleBlur}
+                placeholder="email"
+                className="form-control"
+                required
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                name="password"
+                type="password"
+                onBlur={handleBlur}
+                placeholder="password"
+                className="form-control"
+                required
+              />
+              {newUser && (
+                <div>
+                  <label htmlFor="confirmPassword">Confirm Password</label>
                   <input
-                    {...register("password")}
+                    name="confirmPassword"
                     type="password"
                     onBlur={handleBlur}
                     placeholder="password"
                     className="form-control"
                     required
                   />
-                  <br />
-                  <Button
-                    style={{ backgroundColor: "yellow" }}
-                    type="submit"
-                    className="form-control"
-                  >
-                    Login
-                  </Button>{" "}
-                  <br />
-                  <br />
-                </form>
-                <p>
-                  Don't have an account yet?{" "}
-                  <span
-                    onClick={() => setNewUser(!newUser)}
-                    style={{ fontWeight: "700", cursor: "pointer" }}
-                  >
-                    Sign Up
-                  </span>
-                </p>
-              </div>
+                </div>
+              )}
+              <br />
+              <Button
+                style={{ backgroundColor: "yellow" }}
+                type="submit"
+                className="form-control"
+              >
+                Login
+              </Button>{" "}
+              <br />
+              <br />
+            </form>
+            {user.success ? (
+              <p className="text-success">
+                User {newUser ? "Created" : "Logged In"} Successfully
+              </p>
+            ) : (
+              <p className="text-danger">{user.error}</p>
             )}
             {newUser && (
-              <div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                  <label htmlFor="email">Email</label>
-                  <input
-                    {...register("email")}
-                    type="email"
-                    onBlur={handleBlur}
-                    placeholder="email"
-                    className="form-control"
-                    required
-                  />
-                  <label htmlFor="email">Password</label>
-                  <input
-                    {...register("password")}
-                    type="password"
-                    onBlur={handleBlur}
-                    placeholder="password"
-                    className="form-control"
-                    required
-                  />
-                  <label htmlFor="email">Confirm Password</label>
-                  <input
-                    {...register("confirmPassword")}
-                    type="password"
-                    onBlur={handleBlur}
-                    placeholder="Confirm Password"
-                    className="form-control"
-                    required
-                  />
-                  <br />
-                  <Button
-                    style={{ backgroundColor: "yellow" }}
-                    type="submit"
-                    className="form-control"
-                  >
-                    Sign Up
-                  </Button>{" "}
-                  <br />
-                  <br />
-                </form>
-                <p>
-                  Already Have an account?{" "}
-                  <span
-                    onClick={() => setNewUser(!newUser)}
-                    style={{ fontWeight: "700", cursor: "pointer"}}
-                  >
-                    Login
-                  </span>
-                </p>
-              </div>
+              <p>
+                Already have an account?{" "}
+                <span
+                  onClick={() => setNewUser(!newUser)}
+                  style={{ fontWeight: "700", cursor: "pointer" }}
+                >
+                  Log in
+                </span>
+              </p>
             )}
-          </>
+            {!newUser && (
+              <p>
+                
+                Don't have an account yet?{" "}
+                <span
+                  onClick={() => setNewUser(!newUser)}
+                  style={{ fontWeight: "700", cursor: "pointer" }}
+                >
+                  Sign Up
+                </span>
+              </p>
+            )}
+          </div>
         </div>
         <div className="col-md-3">
           <div className="forgot-pass">
