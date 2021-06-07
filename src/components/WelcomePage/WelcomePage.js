8b259a5eb77ca01b2a0c1eda8398fa2d03b3f916
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-06-07 22:41:19 +0600

    welcome page finished

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -1,10 +1,32 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
+import { useHistory } from 'react-router';
+import image from "../images/authentication.svg"
 
 const WelcomePage = () => {
+
+    let history = useHistory();
+
+    const handleClick = () => {
+        history.push("/")
+    }
+
     return (
         <div className="container">
             <div className="m-5 p-5">
-                <h1 className="text-green">Welcome To You!</h1>
+                <div className="text-center row">
+                    <h1>Welcome To You</h1>
+                    <h1 style={{fontWeight: '800'}} className="text-warning">DOOF</h1>
+                    <div className="my-5">
+                        <h3>Our aim to keep you smile </h3>
+                        <h5>Your Journey Starts from here</h5>
+                    </div>
+                    <h3 className="text-success">Thanks for being with us</h3>
+                    <div>
+                    <Button onClick={handleClick} style={{width: '300px'}} className="text-center" variant="contained" color="secondary">Back to login page</Button>
+                    </div>
+                </div>
+                
             </div>
         </div>
     );
